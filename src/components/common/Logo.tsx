bfba82b19bb06c68ffa28fc354c943953194d7e5
JaydenLee1116 <vv55adss@gmail.com>
2022-11-06 15:05:54 +0900

    refactor: review - 로고 위치, styled components 수정

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -2,15 +2,22 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import theme from 'styles/theme';
+import logo from '../../assets/logo.png';
+
+const Wrapper = styled.div`
+  ${theme.flex.flexBox('row', 'flexstart', 'center')}
+`;
+
 const StyledLogo = styled.img.attrs({
-  src: `${process.env.PUBLIC_URL}/assets/logo.png`,
+  src: `${logo}`,
 })`
   width: 3rem;
   height: 3rem;
 `;
 
-const Wrapper = styled.div`
-  ${theme.flex.flexBox('row', 'flexstart', 'center')}
+const LogoName = styled.div`
+  font-size: ${theme.fontSizes.xl};
+  font-weight: ${theme.fontWeights.extraBold};
 `;
 
 function Logo() {
@@ -23,7 +30,7 @@ function Logo() {
   return (
     <Wrapper onClick={onClick}>
       <StyledLogo />
-      <div style={{ fontSize: '1.5rem', fontWeight: '900' }}>challengers</div>
+      <LogoName>challengers</LogoName>
     </Wrapper>
   );
 }
